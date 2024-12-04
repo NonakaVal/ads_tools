a26d71b6b9a3a7336521d2867971293996b72492
NonakaVal <nonaka96@outlook.com>
2024-12-04 13:36:18 -0300

    dateupade

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ update = st.Page("pages/update.py", title="Atualizar com tabela", icon=":materia
 # labels = st.Page("pages/labels.py", title="Etiquetas Simples", icon=":material/dashboard:")
 # inventario = st.Page("pages_controle/inventario_itens_de_envio.py", title="Inventario", icon=":material/history:")
 # labels = st.Page("pages_controle/labels.py", title="Gerar Etiquetas Simples", icon=":material/dashboard:")
-st.write("atualizado 25-11-24")
+st.write("atualizado 04-12-24")
 anuncio_select = st.Page("pages/item_selector.py", title="Criar Seleção de Produtos", icon=":material/dashboard:")
 # Configuração das páginas
 pg = st.navigation(
